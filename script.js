b54b23902b0cf88ea154329948637b2c225f5484
Bence94x <67744927+Bence94x@users.noreply.github.com>
2021-05-15 18:34:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let theme = localStorage.getItem("theme")
 const bg = document.querySelector('.bg')
 
-let load = 0
+// let load = 0
 
-let int = setInterval(blurring, 30)
+// let int = setInterval(blurring, 30)
 
-function blurring() {
-    load++
+// function blurring() {
+//     load++
 
-    if (load > 99) {
-        clearInterval(int)
-    }
+//     if (load > 99) {
+//         clearInterval(int)
+//     }
 
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
+//     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+// }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+// const scale = (num, in_min, in_max, out_min, out_max) => {
+//     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+// }
 
 if(theme == null){
     setTheme("light")
